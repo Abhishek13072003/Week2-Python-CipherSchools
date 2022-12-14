c8e931d92e5e5bc2d56810a8681ddf8b1ae73b4b
Abhishek Singh <114793975+Abhishek13072003@users.noreply.github.com>
2022-12-15 00:49:55 +0530

    Add files via upload

---
+++ Class 120-Update Dictionary -CipherSchools.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'Abhishek',
+    'age': 19,
+    'fav_movies': ['Twilight'],
+    'fav_tunes': ['horizon','fairy tale'],
+}
+
+more_info= {'state' : 'haryana', 'hobbies' : ['coding','reading','guiter']}
+user_info.update({})
+print(user_info)
